542b8b94ffce5ae82b9f4eb2427fd597ff3e0aa3
NguyenDuong <ntduong10698@gmail.com>
2019-04-14 13:45:03 +0700

    commit

---
+++ js/index.js
@@ -0,0 +1,51 @@
+$(function() {
+    var width = $(window).width();
+    var height = $(window).height();
+
+    $(window).resize(function() {
+        height = $(window).height();
+        $(".menu-mobile").css(`height:${height} !important`)
+    })
+
+    $("ul.list-menu-mobile > li").click(function(){
+        $(this).children(".sub-menu-mobile").toggleClass("view"); //this when click is li .children to select sub-menu-mobile
+    })
+
+    $(".menu-bar .category").click(function() {
+        $(".menu-mobile").removeClass("hidden");
+    })
+    $(".menu-mobile .close-menu-mobile").click(function() {
+        $(".menu-mobile").addClass("hidden");
+    })
+    //end_responsive_mobile
+
+    //menu-service header open -close
+    $(".header .service .menu-service").click(function() {
+        $(".header .service .list-menu").toggleClass("hidden-md");
+    }) //error toggle => click
+    
+    //end responsive header
+    
+    //responsive menu-panel
+    if (width < 767) $(".menu-bar .category span").text("Menu");
+    $(window).resize(function() {
+        width = $(window).width();
+        if (width < 767) {
+            $(".menu-bar .category span").text("Menu"); //change text
+            $(".menu-bar .category i").removeClass("fa-caret-down"); //change icon
+            $(".menu-bar .category i").addClass("fa-bars");
+        } else {
+            $(".menu-bar .category span").text("Danh mục sản phẩm"); //change text
+            $(".menu-bar .category i").removeClass("fa-bars"); //change icon
+            $(".menu-bar .category i").addClass("fa-caret-down");
+        }
+    })
+    //responsive footer
+    $(".footer .list-infor .col-md-3 > div").click(function(event) {
+        let id = event.currentTarget.id; //id of div when click
+        $("#"+id+"> ul").toggleClass("hidden-md");
+        $("#"+id+"> h4").toggleClass("none-border");
+    })
+    //end responsive footer
+
+})
